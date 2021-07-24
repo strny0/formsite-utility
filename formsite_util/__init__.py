dd4f76a05f8efd8e4e964dbb7cef992e929b80c4
Jakub Strnad <jakub.strnad@protonmail.com>
2021-07-24 17:46:54 +0200

    fixed import for split functionality

--- formsite_util/__init__.py
+++ formsite_util/__init__.py
@@ -1,4 +1,5 @@
-from formsite_util.internal.interfaces import FormsiteParams, FormsiteInterface, FormsiteCredentials
+from formsite_util.internal.interfaces import FormsiteParams, FormsiteInterface
+from formsite_util.internal.auth import FormsiteCredentials
 
 # try:
 #     from internal.interfaces import FormsiteParams, FormsiteInterface, FormsiteCredentials
