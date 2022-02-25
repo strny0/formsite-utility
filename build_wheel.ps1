4a6f3d4560945077726f3a56bcbf0c61f459f8cf
Jakub Strnad <jakub.strnad@protonmail.com>
2022-02-25 23:25:19 +0100

    updated poetry config

--- build_wheel.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item -Recurse .\dist\*
-python setup.py sdist bdist_wheel
-twine upload .\dist\*
--- build_wheel.sh
+++
@@ -1,4 +0,0 @@
-rm -r ./dist/*
-python3 setup.py sdist bdist_wheel
-pip3 install twine
-twine upload ./dist/*
