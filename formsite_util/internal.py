c064320e1f95362d5f916349ab0b8827e9056187
Jakub Strnad <jakub.strnad@protonmail.com>
2022-07-17 16:45:20 +0100

    Added callback to be importable

--- formsite_util/internal.py
+++ formsite_util/internal.py
@@ -1,5 +1,5 @@
 from ._form_fetcher import FormFetcher
 from ._form_parser import FormParser
-from ._download_async import AsyncFormDownloader
+from ._download_async import AsyncFormDownloader, AsyncDownloaderCallback
 from ._download import DownloadStatus
 from ._form import FormCallback
